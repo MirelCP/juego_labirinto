82d94403b3ef048c17a95aa52c914affa98c6b6b
Mirel <mirelpisica@gmail.com>
2023-05-14 18:50:52 +0200

    Completo

--- laberinto.js
+++ laberinto.js
@@ -7,6 +7,10 @@ let posY = 1; // posición en y
 
 let laberinto; // laberinto
 
+let tiempoLimite; // Tiempo límite para completar el nivel
+let tiempoRestante; // Tiempo restante para completar el nivel
+let intervaloTiempo; // Identificador del intervalo de tiempo
+
 
 
 
@@ -92,11 +96,12 @@ function setup() {
     noFill();
     noLoop();
 
-    // Establece el laberinto por defecto (puedes escoger cualquiera)
-    laberinto = laberintoFacil;
 }
 
 function keyPressed() {
+    if (laberinto[posY][posX] === 'F') {
+        return; // Salir de la función sin realizar ningún movimiento adicional
+    }
     if (keyCode == UP_ARROW && posY > 0) {
         if (laberinto[posY - 1][posX] == 1 || laberinto[posY - 1][posX] == 'F') {
             posY -= 1;
@@ -117,7 +122,27 @@ function keyPressed() {
             posX += 1;
         }
     }
+    verificarVictoria(); // Verificar si el jugador ha llegado al final después de cada movimiento
+
     redraw();
+}
+
+function verificarVictoria() {
+    if (laberinto[posY][posX] === 'F') {
+        clearInterval(intervaloTiempo); // Detener el contador de tiempo
+        document.getElementById('maze-screen').classList.add('animate__animated', 'animate__bounceIn');
+        document.getElementById('victory-message').style.display = 'block';
+        
+            confetti();
+            
+            setTimeout(() => {
+                document.getElementById('victory-message').style.display = 'none';
+            }, 3000); // Ocultar el mensaje de victoria después de 3 segundos
+            
+          
+        setTimeout(reiniciarJuego, 5000); 
+        
+    }
 }
 
 
@@ -127,28 +152,65 @@ function cambiarNivel() {
 
     if (nivelSeleccionado === "facil") {
         laberinto = laberintoFacil;
+        numCeldas = laberinto.length;
+        tiempoLimite = 30; // 30 segundos para completar el nivel fácil
     } else if (nivelSeleccionado === "medio") {
         laberinto = laberintoMedio;
+        numCeldas = laberinto.length;
+        tiempoLimite = 60; // 60 segundos para completar el nivel medio
     } else if (nivelSeleccionado === "dificil") {
         laberinto = laberintoDificil;
+        numCeldas = laberinto.length;
+        tiempoLimite = 90; // 90 segundos para completar el nivel difícil
     }
+
+    tamCelda = tcanvas / numCeldas;
 
     // Restablecer posición del jugador
     for (let i = 0; i < laberinto.length; i++) {
         for (let j = 0; j < laberinto[i].length; j++) {
             if (laberinto[i][j] === 'J') {
-                posX = j;  
+                posX = j;
                 posY = i;
             }
         }
     }
 
-    numCeldas = laberinto.length;
-    tamCelda = tcanvas / numCeldas;
     clear();
     resizeCanvas(tcanvas, tcanvas);
-    redraw();  // Redibujar el laberinto con el nuevo nivel
+    mostrarTiempoRestante(); // Mostrar el tiempo restante inicial
+    iniciarContadorTiempo(); // Iniciar el contador de tiempo
+    redraw();  // Redibujar el laberinto con el nuevo nivel y tamaño
 }
+
+
+function mostrarTiempoRestante() {
+    let timerElement = document.getElementById("timer");    
+    timerElement.innerHTML = `Tiempo restante: ${tiempoRestante}s`;
+}
+
+function iniciarContadorTiempo() {
+    // Limpiar el intervalo de tiempo anterior si existe
+    if (intervaloTiempo) {
+        clearInterval(intervaloTiempo);
+    }
+
+    tiempoRestante = tiempoLimite;
+    mostrarTiempoRestante();
+
+    intervaloTiempo = setInterval(() => {
+        tiempoRestante--;
+        mostrarTiempoRestante();
+
+        if (tiempoRestante <= 0) {
+            clearInterval(intervaloTiempo);
+            document.getElementById('timeout-message').style.display = 'block';
+            setTimeout(reiniciarJuego, 5000); 
+        }
+        
+    }, 1000);
+}
+
 
 
 
@@ -160,17 +222,19 @@ function empezarJuego() {
 function empezarLaberinto() {
     document.getElementById('level-screen').style.display = 'none';
     document.getElementById('maze-screen').style.display = 'block';
+    cambiarNivel(); // Esta función ya la tienes definida
+    loop(); // Esta función comienza el loop de p5.js para que draw() se llame continuamente
+    verificarVictoria(); // Verificar si el jugador ha llegado al final al inicio del juego
 }
 
-function empezarLaberinto() {
-    document.getElementById('level-screen').style.display = 'none';
-    document.getElementById('maze-screen').style.display = 'block';
-    loop();
-}
+
+
+
 
 function reiniciarJuego() {
     document.getElementById('maze-screen').style.display = 'none';
     document.getElementById('intro-screen').style.display = 'block';
+    document.getElementById('timeout-message').style.display = 'none';
     clear();
     setup();
 }
